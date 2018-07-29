1bb9f9448ad93607f9adda59d0570a17571125f9
AugustoBondanca <augustobondancaa@gmail.com>
2018-07-29 02:55:17 -0300

    create a coutdown

--- src/app/post-tweet/post-tweet.component.ts
+++ src/app/post-tweet/post-tweet.component.ts
@@ -15,11 +15,47 @@ export class PostTweetComponent implements OnInit {
   message;
   private snackBarDuration: number = 2000;
   subscription: Subscription;
-  
+  counter: any;
+  countDownDate = new Date("July 31, 2018 15:37:25").getTime();
+
 
   constructor(private tweetService: TweetService, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+
+    this.startCounter()
+
+  }
+
+  startCounter() {
+
+    setInterval(() => {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = this.countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      this.counter = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+
+
+      // If the count down is over, write some text 
+      // if (distance < 0) {
+      //     clearInterval(x);
+      //     document.getElementById("demo").innerHTML = "EXPIRED";
+      // }
+    }, 1000);
+
   }
 
   messageFormSubmit(messageForm: any) {
